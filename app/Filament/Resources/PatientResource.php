82ae3f614473e9afb047a16a1f36772049d6d575
Jose <josenavaemanuel@outlook.es>
2024-05-08 16:41:29 -0600

    feat: :construction: Creating new owners without leaving the page

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -39,7 +39,23 @@ class PatientResource extends Resource
                     ->maxDate(now()),
                 Forms\Components\Select::make('owner_id')
                     ->relationship('owner', 'name')
-                    ->required(),
+                    ->searchable()
+                    ->preload()
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('phone')
+                            ->label('Phone number')
+                            ->tel()
+                            ->required(),
+                    ])
+                    ->required()
             ]);
     }
 
